621bc3c51d0d3b5c0eea1cb918617754a0ccf045
derwentx <derwentx@gmail.com>
2014-03-10 15:40:51 +0800

    First commit

---
+++ Telephorphor/Telephorphor_receiver/Configuration.h
@@ -0,0 +1,17 @@
+#include <string>
+
+//Common
+#Define RADIO_BAUDRATE 9600
+
+
+//Receiver
+#define rxPin 0
+#define txPin 1
+
+map<string, int[3], string.compare> devices;
+devices["A"] = {3, 5, 6};
+devices["B"] = {9, 10, 11};
+
+
+//Client
+
---
+++ Telephorphor/Telephorphor_receiver/Receiver.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <sstream>
+#include <string>
+
+#include <Configuration.h>
+#include <pwm_colour.h>
+using namespace std;
+
+SoftwareSerial radio = SoftwareSerial(rxPin, txPin);
+
+void setup()
+{
+  radio.begin(RADIO_BAUDRATE);
+}
+
+istringstream radioStream;
+
+void loop()
+{
+  //parse serial input
+  while(radio.available())
+  {
+    char received = radio.read();
+    
+    if(received=='\n'){//?;
+      //get the first substring
+      string sub;
+      radiostream >> sub;
+      
+      //if it is the address of a device being monitored
+      for(deviceInfo::iterator it = devices.begin(); it != devices.end(); ++it){
+        if( sub.compare(it->first) != 0 ){
+          
+      
+      
+      
+      //send response
+        
+      
+    } else {
+      radioStream << received;
+    }
+    
+    
+  }
+    
+  
+  if(commandReady){
+    //modify pwm values
+  }
+}
+  
